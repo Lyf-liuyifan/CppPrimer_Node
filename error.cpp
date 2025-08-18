04c073565d28a0d6874aae2991f833cc3f700371
登风 <853167728@qq.com>
2025-08-18 11:25:09 +0800

    Create error.cpp

---
+++ error.cpp
@@ -0,0 +1,2 @@
+严重性	代码	说明	项目	文件	行	禁止显示状态
+错误	LNK2005	"public: __thiscall Bulk_quote::Bulk_quote(class std::basic_string<char,struct std::char_traits<char>,class std::allocator<char> > const &,double,unsigned int,double)" (??0Bulk_quote@@QAE@ABV?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@NIN@Z) 已经在 Basket.obj 中定义	inheritExcessces	D:\VSC++\Test\inheritExcessces\Basket.obj	1	
